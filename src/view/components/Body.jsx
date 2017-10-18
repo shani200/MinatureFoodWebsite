cdc09825aaba6225d94b85906f077e1038927034
Shani <shani20086@gmail.com>
2017-10-18 17:30:16 +0300

    first-commit

--- src/view/components/Body.jsx
+++ src/view/components/Body.jsx
@@ -12,11 +12,11 @@ export default class Body extends React.Component {
 
     render() {
         return (
-         <div className="body-content">
+         <div className="body-container">
           <NavBar />
-           <div class="explanation">
-            
-                     <div class="content">Bla Bla Bla...</div>
+          <div className="body-content">
+               <img src={'https://i.pinimg.com/564x/12/19/cd/1219cdba2a62d4ae520de28d8d5a9f8b.jpg'} width="1000" height="300" alt="Cactus" className="img" />
+               <div className="explain">Bla Bla Bla...</div>
            </div>
         </div>
         );
