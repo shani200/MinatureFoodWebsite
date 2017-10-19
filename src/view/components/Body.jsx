5706a1ef629373cc70b540de887247bafea9c3b0
Shani <shani20086@gmail.com>
2017-10-19 13:39:38 +0300

    first-commit

--- src/view/components/Body.jsx
+++ src/view/components/Body.jsx
@@ -15,7 +15,7 @@ export default class Body extends React.Component {
          <div className="body-container">
           <NavBar />
           <div className="body-content">
-               <img src={'https://i.pinimg.com/564x/12/19/cd/1219cdba2a62d4ae520de28d8d5a9f8b.jpg'} width="1000" height="300" alt="Cactus" className="img" />
+              <img src={'https://i.pinimg.com/564x/12/19/cd/1219cdba2a62d4ae520de28d8d5a9f8b.jpg'} width="1000" height="300" alt="Cactus" className="img" />
                <div className="explain">
                    Hi,I'm Shani welcome to my website.<br/>
                    I love to make miniatures out of polymer clay, especially minature food.<br/>
--- src/view/components/NavBar.jsx
+++ src/view/components/NavBar.jsx
@@ -14,7 +14,7 @@ export default class NavBar extends React.Component {
             <div className="nav-bar-header">
                 <div className="flex-item"> <div className="login">Hi! Sign in or Register</div> </div>
                 <div className="shopping-cart">
-                    <span>Shooping cart</span>
+                    <img src={'http://findicons.com/files/icons/1700/2d/512/cart.png'} width="200" height="100" alt="shopping-cart" className="img-cart" />
                 </div>
             </div>
         );
