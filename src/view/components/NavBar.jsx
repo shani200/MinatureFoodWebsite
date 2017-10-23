ec9018e0ce0a01c0f4f69cb7fdd27b5a4dabe21d
Shani <shani20086@gmail.com>
2017-10-23 17:24:03 +0300

    first-commit

--- src/view/components/NavBar.jsx
+++ src/view/components/NavBar.jsx
@@ -12,7 +12,7 @@ export default class NavBar extends React.Component {
     render() {
         return (
             <div className="nav-bar-header">
-                <div className="flex-item"> <div className="login">Hi! Sign in or Register</div> </div>
+                <div className="flex-item"> <div className="login"><button className="button ">Hi! Sign in or Register</button></div></div>
                 <div className="shopping-cart">
                     <img src={'http://findicons.com/files/icons/1700/2d/512/cart.png'} width="200" height="100" alt="shopping-cart" className="img-cart" />
                 </div>
