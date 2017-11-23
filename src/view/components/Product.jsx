1344519b4739cc2254ba315f618d4b2711644c08
Shani <shani20086@gmail.com>
2017-11-23 14:24:20 +0200

    first-commit

--- src/view/components/Product.jsx
+++ src/view/components/Product.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../css/product.css'
 import GalleryItem from "./GalleryItem";
-import Cactus from '../pictures/cactus.jpg';
-import flower2 from '../pictures/flower2.jpg';
-import iceCream from '../pictures/iceCream.jpg';
-
+import birhday_cake from '../pictures/birhday_cake.jpg';
+import burn_oven_glove from '../pictures/burn_oven_glove.jpg';
+import cookies from '../pictures/cookies.jpg';
+import blue_set from '../pictures/blue_set.jpg';
+import breads from '../pictures/breads.jpg';
+import blueberry_cake from '../pictures/blueberry_cake.jpg';
+import croissant from '../pictures/croissant.jpg';
+import donuts from '../pictures/donuts.jpg';
+import eclair from '../pictures/eclair.jpg';
+import ice_cream_box from '../pictures/ice_cream_box.jpg';
 
 export default class Product extends React.Component {
     constructor(props) {
@@ -13,30 +19,86 @@ export default class Product extends React.Component {
             isOpen: false,
             galleryItemArr: [
                     {
-                        title: 'title1',
+                        title: 'Confetti Birthday Cake',
                         price: 15.90,
                         desc: 'Handmade item\n '+
                         'Material: polymer clay\n' +
                         ' Gift wrapping and message available',
-                        image: Cactus
+                        image: birhday_cake
                     },
                     {
-                        title: 'title2',
-                        price: 21.90,
+                        title: 'Brown & White Oven Glove',
+                        price: 11.90,
                         desc: 'Handmade item\n '+
                         'Material: polymer clay\n' +
                         ' Gift wrapping and message available',
-                        image: flower2
+                        image: burn_oven_glove
                     },
                     {
-                        title: 'title3',
-                        price: 25.90,
+                        title: 'Cookies',
+                        price: 7.90,
                         desc: 'Handmade item\n '+
                         'Material: polymer clay\n' +
                         ' Gift wrapping and message available',
-                        image: iceCream
-                    }
-                ]
+                        image: cookies
+                    },
+                   {
+                    title: 'Bread Loaves',
+                    price: 12.00,
+                    desc: 'Handmade item\n '+
+                    'Material: polymer clay\n' +
+                    ' Gift wrapping and message available',
+                    image: breads
+                   },
+                   {
+                    title: 'Blue Planner & ice cream & croissant',
+                    price: 15.90,
+                    desc: 'Handmade item\n '+
+                    'Material: polymer clay\n' +
+                    ' Gift wrapping and message available',
+                    image: blue_set
+                   },
+                   {
+                    title: 'Blueberry Cake',
+                    price: 13.90,
+                    desc: 'Handmade item\n '+
+                    'Material: polymer clay\n' +
+                    ' Gift wrapping and message available',
+                    image: blueberry_cake
+                   },
+                   {
+                    title: 'mini croissant',
+                    price: 5.90,
+                    desc: 'Handmade item\n '+
+                    'Material: polymer clay\n' +
+                    ' Gift wrapping and message available',
+                    image: croissant
+                   },
+                   {
+                    title: 'mini Donuts',
+                    price: 7.90,
+                    desc: 'Handmade item\n '+
+                    'Material: polymer clay\n' +
+                    ' Gift wrapping and message available',
+                    image: donuts
+                   },
+                   {
+                    title: 'Eclair',
+                    price: 5.90,
+                    desc: 'Handmade item\n '+
+                    'Material: polymer clay\n' +
+                    ' Gift wrapping and message available',
+                    image: eclair
+                   },
+                   {
+                    title: 'Ice Creams In Little Box',
+                    price: 9.90,
+                    desc: 'Handmade item\n '+
+                    'Material: polymer clay\n' +
+                    ' Gift wrapping and message available',
+                    image: ice_cream_box
+                   }
+            ]
         };
         this.toggleProduct=this.toggleProduct.bind(this);
         this._renderGalleryItems = this._renderGalleryItems.bind(this);
